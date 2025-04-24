916d1de15c4511fcbf0cd9c7632831c2b8b7c204
Harris Ahmad <harris.ahmad@linq.io>
2025-04-24 17:42:44 -0400

    updated to work with cloudlab

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"net/rpc"
@@ -12,13 +13,33 @@ import (
 )
 
 func main() {
-	var addr, pub, cdr, dbFile string
+	var host, port string
+	var pubHost, pubPort string
+	var cdrHost, cdrPort string
+	var dbFile string
 
-	flag.StringVar(&addr, "a", ":1235", "Local address to listen on")
-	flag.StringVar(&pub, "p", ":1235", "Public address reachable by coordinator and other nodes")
-	flag.StringVar(&cdr, "c", ":1234", "Coordinator address")
-	flag.StringVar(&dbFile, "f", "craq.db", "Bolt DB database file")
+	flag.StringVar(&host, "h", "localhost", "Host to bind to")
+	flag.StringVar(&port, "p", "1235", "Port to bind to")
+	flag.StringVar(&pubHost, "ph", "", "Public hostname for other nodes (required)")
+	flag.StringVar(&pubPort, "pp", "", "Public port (defaults to bind port)")
+	flag.StringVar(&cdrHost, "ch", "localhost", "Coordinator hostname")
+	flag.StringVar(&cdrPort, "cp", "1234", "Coordinator port")
+	flag.StringVar(&dbFile, "f", "craq.db", "Database file path")
 	flag.Parse()
+
+	if pubHost == "" {
+		log.Fatal("Public hostname (-ph) is required for cluster deployment")
+	}
+	if pubPort == "" {
+		pubPort = port
+	}
+
+	bindAddr := fmt.Sprintf("%s:%s", host, port)
+	pubAddr := fmt.Sprintf("%s:%s", pubHost, pubPort)
+	cdrAddr := fmt.Sprintf("%s:%s", cdrHost, cdrPort)
+
+	log.Printf("Node binding to %s, advertising as %s", bindAddr, pubAddr)
+	log.Printf("Using coordinator at %s", cdrAddr)
 
 	db := boltdb.New(dbFile, "yessir")
 	if err := db.Connect(); err != nil {
@@ -28,9 +49,9 @@ func main() {
 	defer db.DB.Close()
 
 	n := node.New(node.Opts{
-		Address:           addr,
-		CdrAddress:        cdr,
-		PubAddress:        pub,
+		Address:           bindAddr,
+		CdrAddress:        cdrAddr,
+		PubAddress:        pubAddr,
 		Store:             db,
 		Transport:         netrpc.NewNodeClient,
 		CoordinatorClient: netrpc.NewCoordinatorClient(),
@@ -47,6 +68,6 @@ func main() {
 	go n.Start()
 
 	// Start the rpc server
-	log.Println("Listening at " + addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Println("Listening at " + bindAddr)
+	log.Fatal(http.ListenAndServe(bindAddr, nil))
 }
